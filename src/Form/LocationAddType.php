6209c494bccc9e26df4b9a79a1e46ba49bba75c5
julien-lav <julien.laville@hotmail.fr>
2019-02-01 17:31:07 +0100

    slayer

--- src/Form/LocationAddType.php
+++ src/Form/LocationAddType.php
@@ -31,6 +31,7 @@ class LocationAddType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Location::class,
+            'method' => 'get',
         ]);
     }
 }
